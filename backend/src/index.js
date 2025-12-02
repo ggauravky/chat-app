07b16c06d1648c729e2f78916533cd3480e50c5a
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2025-12-02 12:21:32 +0530

    Add messaging feature with models, routes, and controllers
    Introduces messaging functionality by adding Message model, message routes, and controller methods for sending and retrieving messages. Integrates message routes into the main app and provides endpoints for sidebar user listing, message retrieval, and sending messages.

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
-import { connect } from "mongoose";
+import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
@@ -16,6 +16,7 @@ app.use(cookieParser());
 
 // Routes
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on port :" + PORT);
